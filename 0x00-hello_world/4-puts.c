59ac734dade35687ba68c4d44d2d115cab3b2ea4
Benard Kiplangat <benardkiplangat97@gmail.com>
2023-02-16 16:32:04 -0800

    Task 4, 5, 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: 'the program's description' 
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-char myChar[]="with proper grammar, but the outcome is a piece of art,\n";
+char myChar[] = "\"Programming is like building a multilingual puzzle\n";
 	puts(myChar);
 return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: 'Sizes of types in C'
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 int i;
@@ -9,11 +15,11 @@ double d;
 char c;
 float f;
 
-printf("Size of an char: %lu.\n", (unsigned long).sizeof(c));
-printf("Size of an int: %lu.\n", (unsigned long).sizeof(i));
-printf("Size of an long int: %lu.\n", (unsigned long).sizeof(li));
-printf("Size of a long long int: %lu.\n", (unsigned long).sizeof(lli));
-printf("Size of a float: %lu.\n", (unsigned long).sizeof(f));
+printf("Size of an char: %lu.\n", sizeof(c);
+printf("Size of an int: %lu.\n", sizeof(i);
+printf("Size of an long int: %lu.\n", sizeof(li);
+printf("Size of a long long int: %lu.\n", sizeof(lli);
+printf("Size of a float: %lu.\n", sizeof(f);
 
 return (0);
 }
