c06e6b0f40aa8520c39a8ccfbc9cc5784425cae0
Benard Kiplangat <benardkiplangat97@gmail.com>
2023-02-16 16:43:46 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-char myChar[] = "\"Programming is like building a multilingual puzzle\n";
+char myChar[] = ""Programming is like building a multilingual puzzle\n";
 	puts(myChar);
 return (0);
 }
