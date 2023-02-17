da5c754834f990dbf9ade1460676d423e8fb5fef
Benard Kiplangat <benardkiplangat97@gmail.com>
2023-02-16 16:12:11 -0800

    Task 4, 5, 6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+char myChar[]="with proper grammar, but the outcome is a piece of art,\n";
+	puts(myChar);
+return (0)
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+int i;
+long int li;
+long long int lli;
+double d;
+char c;
+float f;
+
+printf("Size of an char: %lu \n", (unsigned long).sizeof(c));
+printf("Size of an int: %lu \n", (unsigned long).sizeof(i));
+printf("Size of an long int: %lu \n", (unsigned long).sizeof(li));
+printf("Size of a long long int: %lu \n", (unsigned long).sizeof(lli));
+printf("Size of a float: %lu \n", (unsigned long).sizeof(f));
+}
