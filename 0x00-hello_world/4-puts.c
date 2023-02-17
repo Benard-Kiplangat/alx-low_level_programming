2a4625c0a432be8adc0f88dde242144c1ef3104c
Benard Kiplangat <benardkiplangat97@gmail.com>
2023-02-16 16:39:52 -0800

    Task 4, 5, 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point
- * Description: 'the program's description' 
+ * Description: 'the program's description'
  * Return: Always 0 (Success)
  */
 
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,11 +15,11 @@ double d;
 char c;
 float f;
 
-printf("Size of an char: %lu.\n", sizeof(c);
-printf("Size of an int: %lu.\n", sizeof(i);
-printf("Size of an long int: %lu.\n", sizeof(li);
-printf("Size of a long long int: %lu.\n", sizeof(lli);
-printf("Size of a float: %lu.\n", sizeof(f);
+printf("Size of an char: %lu.\n", sizeof(c));
+printf("Size of an int: %lu.\n", sizeof(i));
+printf("Size of an long int: %lu.\n", sizeof(li));
+printf("Size of a long long int: %lu.\n", sizeof(lli));
+printf("Size of a float: %lu.\n", sizeof(f));
 
 return (0);
 }
