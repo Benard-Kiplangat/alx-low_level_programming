8c8e828defe0b88c95d67f19e7ba2d126a5fb693
Benard Kiplangat <benardkiplangat97@gmail.com>
2023-02-16 17:15:21 -0800

    Advanced tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 
 int main(void)
-{	puts("\"Programming is like building a multilingual puzzle");
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,9 +15,9 @@ double d;
 char c;
 float f;
 
-printf("Size of an char: %lu byte(s)\n", sizeof(c));
+printf("Size of a char: %lu byte(s)\n", sizeof(c));
 printf("Size of an int: %lu byte(s)\n", sizeof(i));
-printf("Size of an long int: %lu byte(s)\n", sizeof(li));
+printf("Size of a long int: %lu byte(s)\n", sizeof(li));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
 printf("Size of a float: %lu byte(s)\n", sizeof(f));
 
