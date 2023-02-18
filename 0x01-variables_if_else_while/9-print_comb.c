29e10dbf9d0afebbce3fbf4d8f6ba0cbeac07f00
Benard Kiplangat <benardkiplangat97@gmail.com>
2023-02-18 02:43:26 -0800

    Update for question 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,14 +14,13 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 0; num <10; num++)
 	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
+		putchar(num + '0');
 		putchar(',');
 		putchar(' ');
 	}
+
 	putchar('\n');
 
 	return (0);
