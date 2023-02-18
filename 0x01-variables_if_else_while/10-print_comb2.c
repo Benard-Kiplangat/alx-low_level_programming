1829c083e3ab3069155adaac6b032cec665db62e
Benard Kiplangat <benardkiplangat97@gmail.com>
2023-02-18 03:18:49 -0800

    update to question 10

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -12,18 +12,20 @@
  */
 int main(void)
 {
-	int digit1, digit2;
+	int digit1, digit2 = 0;
 
-	for (digit1 = 0; digit1 < 10; digit1++)
+	for (; digit1 < 10; digit1++)
 	{
-		for (digit2 = 0; digit2 < 10; digit2++)
+		while (digit2 < 10; ++digit2)
 		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
+			putchar(digit1 + '0');
+			putchar(digit2 + '0');
+
 			if (digit1 == 9 && digit2 == 9)
 				break;
+
 			putchar(',');
-		.	putchar(' ');
+			putchar(' ');
 		}
 	}
 
