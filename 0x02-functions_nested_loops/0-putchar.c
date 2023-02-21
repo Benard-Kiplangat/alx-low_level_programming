15657f12c6c5b92fabaa6585cfd5c76ed177dd99
Benard Kiplangat <benardkiplangat97@gmail.com>
2023-02-21 14:44:39 -0800

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+/*
+ * File: 7-print_tebahpla.c
+ * Auth: Benard Kiplangat
+ */
+
+#include "main.h"
+
+/**
+ * main -  prints _putchar, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int _putchar(char c);
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+return (0);
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-alx-low_level_programming
+Variables, IF, WHILE
