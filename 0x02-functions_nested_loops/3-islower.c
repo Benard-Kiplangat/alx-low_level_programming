f42c177f10b7ef51ca4839f3422b465de6e7f905
Benard Kiplangat <benardkiplangat97@gmail.com>
2023-02-21 16:12:20 -0800

    question two

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 /*
  * File: 3-islower.c
- * Auth: Brennan D Baraban
+ * Auth: Benard Kiplangat
  */
 
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _islower - Checks if a character is lowercase.
