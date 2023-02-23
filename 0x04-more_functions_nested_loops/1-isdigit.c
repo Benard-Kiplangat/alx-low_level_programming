22a28799d2106bc1cc5b61045d16f584824fa962
Benard Kiplangat <benardkiplangat97@gmail.com>
2023-02-23 15:43:52 -0800

    Task one and Task two update

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 1` if c is upper, otherwise 0.
+ */
+int _isdigit(int c)
+{
+	int isdig;
+	int isdigit(int a);
+	if (isdigit(c))
+		isdig = 1;
+	else
+		isdig = 0;
+	return (isdig);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,1 +1,4 @@
 int _isupper(int c);
+int isupper(int a);
+int isdigit(int a);
+int _isdigit(int c);
