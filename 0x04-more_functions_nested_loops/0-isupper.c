40156f614a6557a186948c418a3a004d86c890fc
Benard Kiplangat <benardkiplangat97@gmail.com>
2023-02-23 14:52:01 -0800

    Task one

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 1` if c is upper, otherwise 0.
+ */
+int _isupper(int c)
+{
+	if (_isupper(c))
+		return (1);
+	else
+		return (0);
+}
