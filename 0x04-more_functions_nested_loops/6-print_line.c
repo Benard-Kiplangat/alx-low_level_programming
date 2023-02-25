fb6fa596659803869c2830b9ab8fe0c91080d8bf
Benard Kiplangat <benardkiplangat97@gmail.com>
2023-02-25 09:58:51 -0800

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+nclude "main.h"
+
+/**
+* main - check the c
+* Return: Always 0.
+*/
+void print_line(int n)
+{
+	while (i < n)
+	{
+		if (i = 0)
+			_putchar('\n');
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+}
