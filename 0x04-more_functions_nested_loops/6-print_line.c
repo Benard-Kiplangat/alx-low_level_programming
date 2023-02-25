4500102eeac5e090cb2fd851682e80093ad796e2
Benard Kiplangat <benardkiplangat97@gmail.com>
2023-02-25 10:03:47 -0800

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,14 +1,17 @@
-nclude "main.h"
+#include "main.h"
 
 /**
 * main - check the c
 * Return: Always 0.
 */
 void print_line(int n)
+
 {
+	int i = 0;
+
 	while (i < n)
 	{
-		if (i = 0)
+		if (i == 0)
 			_putchar('\n');
 		_putchar('_');
 		i++;
