018a3edaa36c6a3bab518aff57cfcdeba385246e
Benard Kiplangat <benardkiplangat97@gmail.com>
2023-03-09 10:52:54 -0800

    Task 1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,21 +1,22 @@
-#include "main.h" 
-  
- /** 
-   * _puts_recursion - prints a string then a new line 
-   * @s: string to be printed 
-   * Return: void 
-   */ 
-  
- void _puts_recursion(char *s) 
- { 
-         int i = 0; 
-  
-         if (*s == '\0') 
-         { 
-                 _putchar('\n'); 
-         } 
-         else 
-         { 
-                 _putchar(s[i]); 
-                 _puts_recursion(++s); 
-         }
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string then a new line
+ * @s: string to be printed
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(s[i]);
+		_puts_recursion(++s);
+	}
+}
