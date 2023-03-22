5bb9a8c2af886a3f49d196fde847e6bf7b46f4d6
Benard Kiplangat <benardkiplangat97@gmail.com>
2023-03-22 11:39:21 +0300

    a function-like macro ABS(x) that computes the absolute value of a number x

--- 0x0D-preprocessor/0-main.c
+++ 0x0D-preprocessor/0-main.c
@@ -11,6 +11,7 @@
 int main(void)
 {
 	int s;
+
 	s = 98 + SIZE;
 	printf("%d\n", s);
 	return (0);
--- 0x0D-preprocessor/1-main.c
+++ 0x0D-preprocessor/1-main.c
@@ -13,7 +13,7 @@ float a;
 float r;
 
 r = 98;
-a = PI * r * r;
+a = PI * r *r;
 printf("%.3f\n", a);
 return (0);
 }
