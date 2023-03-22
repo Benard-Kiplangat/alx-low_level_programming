aee740af00231f0a2e053145c683f00c530ac230
Benard Kiplangat <benardkiplangat97@gmail.com>
2023-03-22 13:18:27 +0300

    a new type struct dog with the following elements:
    name, type = char *
    age, type = float
    owner, type = char *

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_STRUCT
+#define DOG_STRUCT
+
+/**
+ * struct dog - a new type struct dog with the following elements
+ *
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
