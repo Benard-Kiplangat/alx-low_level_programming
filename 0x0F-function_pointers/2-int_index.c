cc7a13ceb9fc91363f8c22863c6829f700c73e8e
Benard Kiplangat <benardkiplangat97@gmail.com>
2023-03-24 00:07:16 +0300

    Fixing betty in question 1 and pushing question 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - a function that finds an index place of a comparison
+ * @array: an array to compare elements
+ * @size: the number of elements in the array
+ * @cmp: pointer to function used to compare values
+ *
+ * Return: 0 if false, int_index if found, or -1 if size is <= 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	return (-1);
+}
