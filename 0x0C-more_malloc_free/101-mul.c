87c459802553e53c9b9aaca5b1c1fa056361bab4
Benard Kiplangat <benardkiplangat97@gmail.com>
2023-03-26 11:54:22 +0300

    Revision on the last question. Fixing betty errors

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -130,22 +130,19 @@ int main(int argc, char *argv[])
 		_putchar(e[ti]);
 		exit(98);
 	}
-	
-	for (l1 = 0; argv[1][l1]; l1++);
-	for (l2 = 0; argv[2][l2]; l2++);
-
+	for (l1 = 0; argv[1][l1]; l1++)
+		;
+	for (l2 = 0; argv[2][l2]; l2++)
+		;
 	ln = l1 + l2 + 1;
 	a = malloc(ln * sizeof(char));
-
 	if (a == NULL)
 	{
 		for (ti = 0; e[ti]; ti++)
 		_putchar(e[ti]);
 		exit(98);
 	}
-
 	init(a, ln - 1);
-
 	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
 	{
 		t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
@@ -154,8 +151,7 @@ int main(int argc, char *argv[])
 			for (ti = 0; e[ti]; ti++)
 			_putchar(e[ti]);
 			free(a);
-			exit(98);
-		}
+			exit(98);		}
 	}
 	_print(a, ln - 1);
 	return (0);
