54d083d4765f7b1c88282393c002c8f6f3d192f4
benard Kiplangat <benardkiplangat97@gmail.com>
2023-06-08 16:32:23 +0300

    Task 5: square root recursion update

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,22 +1,5 @@
 #include "main.h"
 
-int helper(int n, int j, int i)
-{
-	if (i < 0)
-	{
-		j = -1;
-		return j;
-	}
-	if (i == 0)
-		return (0);
-	else
-	{
-		j++;
-		i = i + 2;
-		return (j + helper(n - i, j, i));	
-	}
-}
-
 /**
  * _sqrt_recursion - a function that returns the
  * natural square root of a given number
@@ -28,15 +11,47 @@ int helper(int n, int j, int i)
 
 int _sqrt_recursion(int n)
 {
+	int upper = n;
+	int lower = 0;
+
 	if (n < 0)
 		return (-1);
 	if (n == 0)
-		return (1);
+		return (0);
 	else
-	{
-		int i = 1;
-		int j = 1;
-		j = j + helper(n, j, i);
-		return (j);
-	}
+		return (helper(n, lower, upper));
+}
+
+/**
+ * helper - a function that helps _sqrt_recursion to calculate the square root
+ *
+ * @n: the number to find its square root
+ * @lower: the number sure to be lower than the sqrt of n
+ * @upper: the number sure to be higher than the sqrt of n
+ *
+ * Return: the natural square root of n or -1
+ */
+
+int helper(int n, int lower, int upper)
+{
+	int middle; /* a number to divide upper and lower for more precision */
+
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	middle = (upper + lower) / 2; /* finding the middle */
+	if (middle * middle == n)
+	/* cheking if the number btwn is the sqrt of n */
+		return (middle);
+	if (upper - lower <= 1)
+	/* breaking the recursion if the sqrt of n become a float */
+		return (-1);
+	if (middle * middle < n)
+		lower = middle;
+	/* if the square of middle is less than n, change lower */
+	else
+		upper = middle;
+	/* else assign it to upper */
+	return (helper(n, lower, upper));
 }
