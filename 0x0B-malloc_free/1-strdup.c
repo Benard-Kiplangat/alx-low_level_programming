3caea9da196d5fa5d48eb949b0ae11f0f7010920
benard Kiplangat <benardkiplangat97@gmail.com>
2023-06-12 18:36:28 +0300

    Task 1: a function that duplicates a string to a new pointer allocated by malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * _strdup - a string that copies a string pointer into
+ *   the memory allocated
+ *
+ * @str: pointer to the string to copy
+ *
+ * Return: a pointer to the string duplicate or NULL
+ */
+
+char *_strdup(char *str)
+{
+	int size = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[size] != '\0')
+	{
+		size++;
+	}
+	if (size > 0)
+	{
+		char *strcpy = (char *)malloc(sizeof(char) * (size + 1));
+		int size2 = 0;
+
+		while (size2 < size)
+		{
+			strcpy[size2] = str[size2];
+			size2++;
+		}
+		strcpy[size2 + 1] = '\0';
+		return (strcpy);
+	}
+	return (NULL);
+}
