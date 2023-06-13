ae4896455a27ea5cef6c473141662509ecd285b7
benard Kiplangat <benardkiplangat97@gmail.com>
2023-06-13 17:14:29 +0300

    Task 3: a program that creates and allocates memory to a multidimensional array, then build grids based on the input variables height and width

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - a function that returns a pointer to a 2
+ * dimensional array of integers
+ *
+ * @width: the width of the grid
+ * @height: the height of the grid
+ *
+ * Return: a pointer to two dimensional array or NULL
+ */
+
+int **alloc_grid(int width, int height)
+{
+	if (height > 0 &&  width > 0)
+	{
+		int i = 0;
+		int **grid = malloc(height * sizeof(int *));
+
+		for(i = 0; i < height; i++)
+			grid[i] = malloc(width * sizeof(int));
+
+		if (grid == NULL)
+			return (NULL);
+		return (grid);
+	}
+	return (NULL);	
+}
