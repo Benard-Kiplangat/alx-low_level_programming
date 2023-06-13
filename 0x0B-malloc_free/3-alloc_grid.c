c92ef1d37a6e7429affa6079f0dd42ec72250c11
benard Kiplangat <benardkiplangat97@gmail.com>
2023-06-13 17:32:35 +0300

    Released memory if malloc fails when allocating memory to a row

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,6 +27,11 @@ int **alloc_grid(int width, int height)
 			grid[i] = malloc(width * sizeof(int));
 			if (grid[i] == NULL)
 			{
+				while (i)
+				{
+					free(grid[i - 1]);
+					i--;
+				}
 				free(grid);
 				return (NULL);
 			}
