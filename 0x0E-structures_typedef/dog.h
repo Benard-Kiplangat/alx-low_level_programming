1abeddf940567fc4a8f011cf00299b7531270356
benard Kiplangat <benardkiplangat97@gmail.com>
2023-06-19 21:57:21 +0300

    Task 0: an header file that defines a new type struct dog with the name, age, and owner as its elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct for a dog
+ * 
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: a struct that describes a dog's name, age, and owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
