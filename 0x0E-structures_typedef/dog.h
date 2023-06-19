49e599920405928f8f1399e8eb2163d83201c7d5
benard Kiplangat <benardkiplangat97@gmail.com>
2023-06-19 22:35:25 +0300

    Task 3: typedef definition of struct dog with a new type dog_t as a new name

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,12 +11,12 @@
  * Description: a struct that describes a dog's name, age, and owner
  */
 
-struct dog
+typedef struct dog
 {
-	char *name;
-	float age;
-	char *owner;
-};
+char *name;
+float age;
+char *owner;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
