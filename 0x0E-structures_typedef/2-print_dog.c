87d6607c26158f54839348b9007a1c0c66932839
benard Kiplangat <benardkiplangat97@gmail.com>
2023-06-20 20:36:07 +0300

    Task 2: adding a newline character to printf function for age

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		printf("\n");
+		printf(" ");
 	else
 	{
 		if (d->name == NULL)
@@ -20,7 +20,7 @@ void print_dog(struct dog *d)
 		else
 			printf("Name: %s\n", d->name);
 		if (!(d->age))
-			printf("Age: (nil)");
+			printf("Age: (nil)\n");
 		else
 			printf("Age: %f\n", d->age);
 		if (d->owner == NULL)
