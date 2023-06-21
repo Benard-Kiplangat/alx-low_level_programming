7836e5d8c0853ac0c6c9ab0314798313ba8e25af
benard Kiplangat <benardkiplangat97@gmail.com>
2023-06-21 16:39:22 +0300

    Task 0: a function that prints a name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef FUNC_PTR
 #define FUNC_PTR
+#include <stddef.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
