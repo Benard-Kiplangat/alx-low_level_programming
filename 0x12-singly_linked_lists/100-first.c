1ec9e3148c24afb6dd48e13a93e3dc5fbed05148
benard Kiplangat <benardkiplangat97@gmail.com>
2023-06-30 20:06:09 +0300

    Fixing betty errors: no continuation of strings on the next line

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,7 +10,7 @@ int func(void) __attribute__((constructor));
 
 int func(void)
 {
-	printf("You're beat! and yet, you must allow,\n\
-I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 	return (0);
 }
