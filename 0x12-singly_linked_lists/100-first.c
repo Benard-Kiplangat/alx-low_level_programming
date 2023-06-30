fd93fc2564cc48c6a40b0d7eb2a63c723b8b8d3c
benard Kiplangat <benardkiplangat97@gmail.com>
2023-06-30 19:56:12 +0300

    Fixing betty errors: line over 80 characters and func description

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,15 +1,16 @@
 #include "lists.h"
 
 /**
- *  - a function that prints You're beat! and yet, you must allow,\nI
+ * func - a function that prints You're beat! and yet, you must allow,\nI
  *  bore my house upon my back!\n
- * 
+ *
  * Return: nothing
  */
 int func(void) __attribute__((constructor));
 
 int func(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n\
+I bore my house upon my back!\n");
 	return (0);
 }
