135ff336112673dc7a9ed5facd32d53bac43bff7
benard Kiplangat <benardkiplangat97@gmail.com>
2023-07-10 19:20:36 +0300

    Task 2: a program that appends text to a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,6 +9,36 @@
  * Return: 1 on success and -1 on failure
  */
 
-int append_text_to_file(condt char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
+	int opn, i;
+	size_t opn2, sz;
+
+	if (filename == NULL)
+		return (-1);
+
+	opn = open(filename, O_WRONLY | O_CREAT | O_APPEND, 00600);
+
+	if (opn < 0)
+		return (-1);
+
+	if (opn && !text_content)
+	{
+		close(opn);
+		return (1);
+	}
+
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
+
+	sz = sizeof(char) * i;
+
+	opn2 = write(opn, text_content, sz);
+
+	if (opn2 > 0)
+	{
+		close(opn2);
+		return (1);
+	}
+	return (-1);
 }
