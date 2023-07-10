90a1d34eb81568a3db049be4b334e914ef064fc4
benard Kiplangat <benardkiplangat97@gmail.com>
2023-07-10 20:16:11 +0300

    Task 3: a function that copies content from one file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,6 +9,52 @@
  * Return: 0 on success and -1 on failure
  */
 
-int cp(int argc, char **argv)
+int main(int argc, char **argv)
 {
+	char *buffer = malloc(sizeof(char) * 1024);
+	size_t opn, opn2, i = 0, j = 0;
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	if (access(argv[2], F_OK) == 0)
+	{
+		opn = open(argv[1], 1024 * 5);
+
+		while (argv[1][i] != '\0')
+		{
+			for (; argv[1][i] != '\0' || i < 1024; i++)
+			{
+				if (i == 1023)
+				{
+					buffer[i + 1] = '\0';
+					j = 0;
+					break;
+				}
+				buffer[j] = argv[1][i];
+				j++;
+			}
+			opn2 = write(opn, buffer, O_TRUNC | O_WRONLY);
+			if (opn2 <= 0)
+			{
+				dprintf(2, "Error: Can't write to %s\n", argv[2]);
+				exit(99);
+			}
+			if (close(opn) < 0)
+			{
+				dprintf(2, "Error: Can't close %li\n", opn);
+				exit(100);
+			}
+	else
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+}
+}
+return (0);
 }
