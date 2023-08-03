059453b5392510adf0b7258d47b66e04cbee4ad8
benard Kiplangat <benardkiplangat97@gmail.com>
2023-08-03 22:30:13 +0300

    Fixed betty errors and added testfiles

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -4,6 +4,7 @@
  * clear_bit - a function to set the value of a bit to 0 at a given index
  *
  * @n: the number to clear its bit
+ * @index: the index to clear its bit
  *
  * Return: 1 if it works or -1 on error
  */
@@ -17,7 +18,9 @@ int clear_bit(unsigned long int *n, unsigned int index)
 
 	if (index > sizeof(unsigned long int) * 8 || n == NULL)
 		return (-1);
+
 	if (((*n >> index) & 1) == 1)
 		*n = mask ^ *n;
+
 	return (1);
 }
