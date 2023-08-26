23718705edd3ae3caab45520f3e4ae4ec38f41b2
benard Kiplangat <benardkiplangat97@gmail.com>
2023-08-26 11:44:40 +0300

    included a custom implementation of strlen function

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * _strlen - compute the length of a NULL-terminated string
+ * @str: the string to measure
+ *
+ * Return: the length of str, or -1 if str is NULL
+ */
+ssize_t _strlen(const char *str)
+{
+	ssize_t len = 0;
+
+	if (!str)
+		return (-1);
+
+	while (*str++)
+		++len;
+
+	return (len);
+}
+
+/**
  * create_file - a function that creates a file
  *
  * @filename: the name of the new file
@@ -11,27 +30,23 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int opn, i;
-	size_t opn2, sz;
+	ssize_t b_written = 0;
+	int fd;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	opn = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
-	if (opn < 0)
+	if (fd < 0)
 		return (-1);
 
-	for (i = 0; text_content[i] != '\0'; i++)
-		;
+	if (text_content)
+		b_written = write(fd, text_content, _strlen(text_content));
 
-	sz = sizeof(char) * i;
+	close(fd);
 
-	opn2 = write(opn, text_content, sz);
-	if (opn2 > 0)
-	{
-		close(opn2);
-		return (1);
-	}
-	return (-1);
+	if (b_written < 0)
+		return (-1);
+	return (1);
 }
