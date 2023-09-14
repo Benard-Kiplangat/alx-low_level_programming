6dcd49dbfc90c29d52c73ac0b54298f8fb8b0b71
benard Kiplangat <benardkiplangat97@gmail.com>
2023-09-14 21:28:52 +0300

    Task 1: a function that counts all the elements of a linked list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,18 +1,17 @@
 #include "lists.h"
 
 /**
- * print_dlistint - a function that prints elements of a doubly linked list
+ * dlistint_len - a function that counts elements of a doubly linked list
  * @h: a dlistint list to print its elements
  * Return: the number of elements
  */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
-	int n = 0;
+	size_t n = 0;
 
 	while (h)
 	{
-		printf("%d\n", h->n);
 		h = h->next;
 		n++;
 	}
