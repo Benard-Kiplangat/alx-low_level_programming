34293ee48a75964b7ac4869936e0379fa1319c92
benard Kiplangat <benardkiplangat97@gmail.com>
2023-09-17 19:28:23 +0300

    Fixing when the next node is null and index is less that i

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,6 +28,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	for (i = 0; k != NULL && i < idx; i++)
 		k = k->next;
+	if (k == NULL && i < idx)
+	{
+		k = nwnode;
+		nwnode->next = NULL;
+		return (nwnode);
+	}
 	nwnode->next = k;
 	nwnode->prev = k->prev->prev;
 	k->prev->next = nwnode;
