8b2792532497796595e44a6fcc650d94a1e88a1a
benard Kiplangat <benardkiplangat97@gmail.com>
2023-10-06 21:46:28 +0300

    Task 1: a function that implements djb algorithim

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,11 @@
+#include "hash_tables"
+
+/**
+ * hash_djb2 - a function that implements djb algorithim
+ * @str: the string to create its hash
+ * Return: the created hash
+ */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
     unsigned long int hash;
